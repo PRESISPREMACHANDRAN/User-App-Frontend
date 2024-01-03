648fdaf58257bbb6a398312b8d951bd3c4e57adc
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-03 22:23:37 +0530

    user app created

---
+++ src/Components/AddData.jsx
@@ -0,0 +1,306 @@
+import React from 'react'
+import Header from './Header'
+
+const AddData = () => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  ID
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  First Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  Last Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  maiden Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  Age
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  Gender
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  email
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  phone
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  username
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  password
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  birthDate
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  image
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  bloodGroup
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  height
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  weight
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  eyeColor
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  hair color
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  hair type
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  domain
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  ip
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  address
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  city
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  coordinates latitude
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  coordinates longitude
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  postalCode
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  state.
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  mac address
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  university
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  cardExpire
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  cardNumber
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  cardType
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  currency
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  iban
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  company address
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  company city
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY ADDRESS COORDINATES LATITUDE
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY ADDRESS COORDINATES LATITUDE
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY ADDRESS COORDINATES LONGITUDE
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY ADDRESS POSTCODE
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY ADDRESS STATE
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY ADDRESS STATE
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY DEPARTMENT
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY NAME
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  COMPANY TITLE
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  ein
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  ssn
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <label htmlFor="" className="form-label">
+                  user agent
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success">ADD</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddData
---
+++ src/Components/ViewAll.jsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import axios from "axios";
+
+const ViewAll = () => {
+  const [userData, changeUserData] = useState({
+users:[],
+    total: 100,
+    skip: 0,
+    limit: 30,
+  });
+
+  const fetchData=()=>{
+axios.get("https://dummyjson.com/users")
+.then(
+  (response)=>{
+    changeUserData(response.data)
+  }
+)
+  }
+  useEffect(()=>{fetchData()},[])
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">ID</th>
+                      <th scope="col">firstName</th>
+                      <th scope="col">lastName</th>
+                      <th scope="col">maidenName</th>
+                      <th scope="col">age</th>
+                      <th scope="col">GENDER</th>
+                      <th scope="col">email</th>
+                      <th scope="col">phone</th>
+                      <th scope="col">username</th>
+                      <th scope="col">password</th>
+                      <th scope="col">birth date</th>
+                      <th scope="col">image</th>
+                      <th scope="col">Blood group</th>
+                      <th scope="col">height</th>
+                      <th scope="col">weight</th>
+                      <th scope="col">eye color</th>
+                      <th scope="col">hair color</th>
+                      <th scope="col">hair type</th>
+                      <th scope="col">domain</th>
+                      <th scope="col">ip</th>
+                      <th scope="col">address</th>
+                      <th scope="col">city</th>
+                      <th scope="col">coordinates.lat</th>
+                      <th scope="col">coordinates.lng</th>
+                      <th scope="col">postalCode</th>
+                      <th scope="col">state.</th>
+                      <th scope="col">macAddress</th>
+                      <th scope="col">university</th>
+                      <th scope="col">cardExpire</th>
+                      <th scope="col">cardNumber</th>
+                      <th scope="col">cardType</th>
+                      <th scope="col">currency</th>
+                      <th scope="col">iban</th>
+                      <th scope="col">company.address.address</th>
+                      <th scope="col">company.address.city</th>
+                      <th scope="col">company.address.coordinates.lat</th>
+                      <th scope="col">company.address.coordinates.lng</th>
+                      <th scope="col">company.address.postalcode</th>
+                      <th scope="col">company.address.state</th>
+                      <th scope="col">company.department</th>
+                      <th scope="col">company.name</th>
+                      <th scope="col">company.title</th>
+                      <th scope="col">ein</th>
+                      <th scope="col">ssn</th>
+                      <th scope="col">useragent</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {userData.users.map((value, index) => {
+                      return (
+                        <tr>
+                          <th scope="row">{value.id}</th>
+                          <td>{value.firstName}</td>
+                          <td>{value.lastName}</td>
+                          <td>{value.maidenName}</td>
+                          <td>{value.age}</td>
+                          <td>{value.gender}</td>
+                          <td>{value.email}</td>
+                          <td>{value.phone}</td>
+                          <td>{value.username}</td>
+                          <td>{value.password}</td>
+                          <td>{value.birthDate}</td>
+
+                         <td> <img
+                            src={value.image}
+                            alt="imag"
+                            
+                          /></td>
+                          <td>{value.bloodGroup}</td>
+                          <td>{value.height}</td>
+                          <td>{value.weight}</td>
+                          <td>{value.eyeColor}</td>
+                          <td>{value.hair.color}</td>
+                          <td>{value.hair.type}</td>
+                          <td>{value.domain}</td>
+                          <td>{value.ip}</td>
+                          <td>{value.address.address}</td>
+                          <td>{value.address.city}</td>
+                          <td>{value.address.coordinates.lat}</td>
+                          <td>{value.address.coordinates.lng}</td>
+                          <td>{value.address.postalCode}</td>
+                          <td>{value.address.state}</td>
+                          <td>{value.macAddress}</td>
+                          <td>{value.university}</td>
+                          <td>{value.bank.cardExpire}</td>
+                          <td>{value.bank.cardNumber}</td>
+                          <td>{value.bank.cardType}</td>
+                          <td>{value.bank.currency}</td>
+                          <td>{value.bank.iban}</td>
+                          <td>{value.company.address.address}</td>
+                          <td>{value.company.address.city}</td>
+                          <td>{value.company.address.coordinates.lat}</td>
+                          <td>{value.company.address.coordinates.lng}</td>
+                          <td>{value.company.address.postalCode}</td>
+                          <td>{value.company.address.state}</td>
+                          <td>{value.company.department}</td>
+                          <td>{value.company.name}</td>
+                          <td>{value.company.title}</td>
+                          <td>{value.ein}</td>
+                          <td>{value.ssn}</td>
+                          <td>{value.userAgent}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewAll;
